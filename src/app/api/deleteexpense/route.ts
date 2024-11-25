255d4b1c043dc9d13a1f0c51fc1aaf6d725c2fa1
thititongumpun <thiti180536@gmail.com>
2024-11-25 23:41:57 +0700

    add kv

--- src/app/api/deleteexpense/route.ts
+++
@@ -1,8 +0,0 @@
-import { getStore } from "@netlify/blobs";
-
-export async function DELETE(id: string) {
-  const expense = getStore({ siteID: process.env.BLOB_SITE_ID, token: process.env.BLOB_SITE_KEY, name: "expense" });
-  await expense.delete(id)
-
-  return Response.json({ message: "delete success" })
-}
